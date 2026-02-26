c8d677276c3ff6bc83f53a658fb02cb32c9bc160
Dohyeop Lim <dhlim0817@gmail.com>
2026-02-26 22:56:32 +0900

    feat: add right intro

--- src/app/sections/Intro/_components/Profile.tsx
+++ src/app/sections/Intro/_components/Profile.tsx
@@ -1,1 +1,27 @@
+import Image from "next/image";
 
+export default function Profile() {
+    return (
+        <figure className="font-caption03-regular text-grey-500 uppercase w-full max-w-43.75">
+            <figcaption className="flex justify-between">
+                <span>Portrait</span>
+                <span>2026</span>
+            </figcaption>
+
+            <div className="relative w-full aspect-175/230 mt-1.5">
+                <Image
+                    src="/profile.png"
+                    alt="Dohyeop Lim's profile image"
+                    fill
+                    className="object-cover"
+                    sizes="(max-width: 768px) 40vw, 175px"
+                />
+            </div>
+
+            <figcaption className="flex justify-between mt-1.5">
+                <span>Fig. 01</span>
+                <span>Seoul, KR</span>
+            </figcaption>
+        </figure>
+    );
+}
--- src/app/sections/Intro/index.tsx
+++ src/app/sections/Intro/index.tsx
@@ -1,13 +1,16 @@
 import Bio from "./_components/Bio";
 import Name from "./_components/Name";
+import Profile from "./_components/Profile";
 
 export default function Intro() {
     return (
-        <section>
+        <section className="w-full flex items-center justify-between">
             <div className="flex flex-col gap-15">
                 <Name />
                 <Bio />
             </div>
+
+            <Profile />
         </section>
     );
 }
