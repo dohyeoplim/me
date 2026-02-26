c827f2d60bbf8ca2d37f785e896ff8bd6752f6d3
Dohyeop Lim <dhlim0817@gmail.com>
2026-02-26 22:29:38 +0900

    feat: add footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="w-full max-w-4xl mx-auto px-6 select-none pointer-events-none">
+            <hr className="h-px text-grey-200 mb-6" />
+            <small className="font-body04-light text-grey-500">
+                &copy; 2026 Dohyeop Lim
+            </small>
+        </footer>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { pretendard } from "@/assets/fonts/pretendard";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
     title: "Dohyeop Lim",
@@ -19,6 +20,7 @@ export default function RootLayout({
             <body className={`${pretendard.variable} antialiased`}>
                 <Header />
                 {children}
+                <Footer />
             </body>
         </html>
     );
