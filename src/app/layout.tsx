a47de986664dfd7c3504939fa5c190e712e0d59f
Dohyeop Lim <dhlim0817@gmail.com>
2026-02-27 16:00:50 +0900

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { pretendard } from "@/assets/fonts/pretendard";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
     title: "Dohyeop Lim",
@@ -25,6 +26,7 @@ export default function RootLayout({
                 <Header />
                 {children}
                 <Footer />
+                <Analytics />
             </body>
         </html>
     );
