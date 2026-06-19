0ff806b0ab0fdd3e7f5c560c43fe149e8507e57d
Dohyeop Lim <dhlim0817@gmail.com>
2026-06-19 22:38:33 +0900

    fix: enable local IP access for images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     },
     images: {
         unoptimized: process.env.NODE_ENV === "development",
+        dangerouslyAllowLocalIP: true,
         remotePatterns: [
             {
                 protocol: "https",
