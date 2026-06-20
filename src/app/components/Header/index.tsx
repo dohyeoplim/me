00280e6237b01cd73966c9514f75d05094f5385e
Dohyeop Lim <dhlim0817@gmail.com>
2026-06-20 15:46:50 +0900

    style: reduce header top padding on desktop

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -33,7 +33,7 @@ export default function Header() {
                 />
                 <div className="absolute inset-0 bg-linear-to-b from-grey-50/75 to-grey-50/0" />
             </div>
-            <div className="relative max-w-4xl mx-auto px-6 pt-4 md:pt-16 pb-2 md:pb-4">
+            <div className="relative max-w-4xl mx-auto px-6 pt-4 md:pt-10 pb-2 md:pb-4">
                 <nav className="pointer-events-auto w-full flex items-center justify-between">
                     <Link href={href} transitionTypes={["nav-back"]}>
                         <Item label={label} className="select-none" />
